02b177c6477b9e419e697772f36d3e6c67e77c23
OyedunOye <oyesinaoyedun>
2023-12-16 12:53:28 +0100

    menu section structure, pending finishing touches

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -13,8 +13,8 @@ const SpecialMenu = () => (
       <div className='app__specialMenu-menu_wine'>
         <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
         <div className='app__specialMenu-menu_items'>
-          {data.cocktails.map((cocktails, index)=> (
-            <p>{cocktails.title}</p>
+          {data.wines.map((wine, index)=> (
+            < MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
           ))}
         </div>
       </div>
@@ -26,15 +26,16 @@ const SpecialMenu = () => (
       <div className='app__specialMenu-menu_cocktails'>
         <p className='app__specialMenu-menu_heading'>Cocktails</p>
         <div className='app__specialMenu-menu_items'>
-          {data.wines.map((wine, index)=> (
-            <p>{wine.title}</p>
+          {data.cocktails.map((cocktail, index)=> (
+            < MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
+
           ))}
         </div>
       </div>
     </div>
 
     <div style={{marginTop: '15px'}}>
-      <button type='button' className='custom__button'>Order</button>
+      <button type='button' className='custom__button'>View more</button>
     </div>
   </div>
 );
