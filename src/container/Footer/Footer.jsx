c61d50ef52fa0524ca035200645b3673fae4edf5
OyedunOye <oyesinaoyedun@yahoo.com>
2023-12-23 21:09:11 +0100

    layout set out for footer section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { FiFacebook, FiInstagram, FiTwitter } from 'react-icons/fi'
 import './Footer.css';
+import FooterOverlay from './FooterOverlay';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay />
   </div>
 );
 
