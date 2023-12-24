314e29d3b07e2805448b8a4a21cab05cc469f3a4
OyedunOye <oyesinaoyedun@yahoo.com>
2023-12-24 21:54:16 +0100

    footer section in progress

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 import './Newsletter.css';
+import SubHeading from '../SubHeading/SubHeading';
 
 const Newsletter = () => (
-  <div>
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title='Newsletter' />
+      <h1 className='headtext__cormorant'>Subscribe to our newsletter</h1>
+      <p className='p__opensans'>And never miss latest updates</p>
+    </div>
+    <div className='app__newsletter-input  flex__center'>
+      <input type='email' placeholder='Enter your email address' />
+      <button className='Custom__button' type='button'>Subscribe</button>
+    </div>
     Newsletter
   </div>
 );
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FiFacebook, FiInstagram, FiTwitter } from 'react-icons/fi'
 import './Footer.css';
-import FooterOverlay from './FooterOverlay';
+import  { FooterOverlay, Newsletter } from '../../components';
 
 const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
